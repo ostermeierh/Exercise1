87d9626882ab9a74cded3ba1dcda92e1061172ab
Heinrich Ostermeier <ga24diq@mytum.de>
2017-11-09 16:07:20 +0100

    //changed HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 ////
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
-		System.out.println("Hello ASE2017 - How are you? :)");
+		HelloUser helloUser = new HelloUser();
+		
+		System.out.println("Hello ASE2017 - How are you now? :)");
+		System.out.print("Please enter your name: ");
+		String name = new Scanner(System.in).nextLine();
+		helloUser.setUserName(name);
+		helloUser.printAnswer();
 	}
 
+	
 }
